8c522c6c4325f1d564fdcafa1d66de2f1aae4e63
=Phong Bui <phongtrank55@gmail.com>
2021-06-04 13:05:32 +0700

    test

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,5 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/social/{provider}', 'Auth\LoginController@redirect')->name('auth.social');
+Route::get('/callback/{provider}', 'Auth\LoginController@callback')->name('auth.social.callback');
