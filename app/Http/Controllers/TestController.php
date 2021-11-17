404e9722fbd47a9396efa8cdd8ae97ec79847f03
phongtrank55@gmail.com <phongtrank55@gmail.com>
2021-11-17 08:19:14 +0700

    test

--- app/Http/Controllers/TestController.php
+++ app/Http/Controllers/TestController.php
@@ -10,7 +10,7 @@ class TestController extends Controller
     public function index(){
         $article = Article::find(1);
         $article->title = 'TEST 1 Nên mua Tivi nào tốt nhất phù hợp với gia đình bạn năm 2017';
-        $article->body = 'Test 1 khiển tivi bằng điện thoại với ứng dụng LG dam bảo khả năng tiết kiệm điện năng tối ưu.';
+        $article->body = 'Test 1 tivi bằng thoại với ứng dụng LG dam bảo khả năng tiết kiệm điện năng tối ưu.';
         $article->save();
         $article = Article::find(2);
         $article->title = 'TEST 2 Nên mua Tivi ';
@@ -19,6 +19,22 @@ class TestController extends Controller
         $article = Article::find(3);
         $article->title = 'TEST 3 Nên mua ';
         $article->body = 'test 3 bằng điện thoại với ứng dụng tiết kiệm tối ưu.';
+        $article->save();
+        $article = Article::find(4);
+        $article->title = '5tr';
+        $article->body = 'accđ';
+        $article->save();
+        $article = Article::find(5);
+        $article->title = 'Phone with LG application is an energy saving tv';
+        $article->body = 'xxxx';
+        $article->save();
+        $article = Article::find(6);
+        $article->title = 'That tv made by energy saving LG applications';
+        $article->body = 'xxxx';
+        $article->save();
+        $article = Article::find(7);
+        $article->title = 'TEST 7 Phone with LG application ensures optimal energy saving';
+        $article->body = 'xxx';
         $article->save();
         return 'ok done';
     }
